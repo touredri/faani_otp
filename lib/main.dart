93a084bf9402e6f5bb7ee6b0f85fa817b096d037
touredri <dcompte223@gmail.com>
2023-11-23 20:08:44 +0000

    uodate main

--- lib/main.dart
+++ lib/main.dart
@@ -139,7 +139,7 @@ void onStart(ServiceInstance service) async {
 
         // if you don't using custom notification, uncomment this
         service.setForegroundNotificationInfo(
-          title: "My App Service",
+          title: "Faani OTP Service",
           content: "Updated at ${DateTime.now()}",
         );
       }
@@ -185,113 +185,82 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Service App'),
+          title: const Text('Faani OTP Service'),
+          centerTitle: true,
         ),
-        body: Column(
-          children: [
-            StreamBuilder<Map<String, dynamic>?>(
-              stream: FlutterBackgroundService().on('update'),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
+        body: Center(
+          child: Column(
+            children: [
+              StreamBuilder<Map<String, dynamic>?>(
+                stream: FlutterBackgroundService().on('update'),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  final data = snapshot.data!;
+                  String? device = data["device"];
+                  DateTime? date = DateTime.tryParse(data["current_date"]);
+                  return Column(
+                    children: [
+                      Text(device ?? 'Unknown'),
+                      Text(date.toString()),
+                    ],
                   );
-                }
+                },
+              ),
+              ElevatedButton(
+                child: const Text("Foreground Mode"),
+                onPressed: () {
+                  FlutterBackgroundService().invoke("setAsForeground");
+                },
+              ),
+              ElevatedButton(
+                child: const Text("Background Mode"),
+                onPressed: () {
+                  FlutterBackgroundService().invoke("setAsBackground");
+                },
+              ),
+              ElevatedButton(
+                child: Text(text),
+                onPressed: () async {
+                  final service = FlutterBackgroundService();
+                  var isRunning = await service.isRunning();
+                  if (isRunning) {
+                    service.invoke("stopService");
+                  } else {
+                    service.startService();
+                  }
 
-                final data = snapshot.data!;
-                String? device = data["device"];
-                DateTime? date = DateTime.tryParse(data["current_date"]);
-                return Column(
-                  children: [
-                    Text(device ?? 'Unknown'),
-                    Text(date.toString()),
-                  ],
-                );
-              },
-            ),
-            ElevatedButton(
-              child: const Text("Foreground Mode"),
-              onPressed: () {
-                FlutterBackgroundService().invoke("setAsForeground");
-              },
-            ),
-            ElevatedButton(
-              child: const Text("Background Mode"),
-              onPressed: () {
-                FlutterBackgroundService().invoke("setAsBackground");
-              },
-            ),
-            ElevatedButton(
-              child: Text(text),
-              onPressed: () async {
-                final service = FlutterBackgroundService();
-                var isRunning = await service.isRunning();
-                if (isRunning) {
-                  service.invoke("stopService");
-                } else {
-                  service.startService();
-                }
-
-                if (!isRunning) {
-                  text = 'Stop Service';
-                } else {
-                  text = 'Start Service';
-                }
-                setState(() {});
-              },
-            ),
-            const Expanded(
-              child: LogView(),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.play_arrow),
+                  if (!isRunning) {
+                    text = 'Stop Service';
+                  } else {
+                    text = 'Start Service';
+                  }
+                  setState(() {});
+                },
+              ),
+              // const Expanded(
+              //   child: LogView(),
+              // ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class LogView extends StatefulWidget {
-  const LogView({Key? key}) : super(key: key);
 
-  @override
-  State<LogView> createState() => _LogViewState();
-}
-
-class _LogViewState extends State<LogView> {
-  late final Timer timer;
-  List<String> logs = [];
-
-  @override
-  void initState() {
-    super.initState();
-    timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
-      final SharedPreferences sp = await SharedPreferences.getInstance();
-      await sp.reload();
-      logs = sp.getStringList('log') ?? [];
-      if (mounted) {
-        setState(() {});
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    timer.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: logs.length,
-      itemBuilder: (context, index) {
-        final log = logs.elementAt(index);
-        return Text(log);
-      },
-    );
-  }
-}
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//       itemCount: logs.length,
+//       itemBuilder: (context, index) {
+//         final log = logs.elementAt(index);
+//         return Text(log);
+//       },
+//     );
+//   }
+// }
